a33315963349affd34a4140296aadc10b0d5f5c2
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-07 15:14:44 +0000

    Main game class setup

--- core/src/com/neves6/piazzapanic/PiazzaPanicGame.java
+++ core/src/com/neves6/piazzapanic/PiazzaPanicGame.java
@@ -1,31 +1,39 @@
 package com.neves6.piazzapanic;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class PiazzaPanicGame extends ApplicationAdapter {
+public class PiazzaPanicGame extends Game {
 	SpriteBatch batch;
-	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		setScreen(new IntroScreen(this));
+	}
+
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	public void render(float delta) {
+		super.render();
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
