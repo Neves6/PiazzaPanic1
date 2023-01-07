ca71fc0fa9a190b23e3778964d12f4508b9f4e25
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-07 19:52:29 +0000

    Main game class import refactor

--- core/src/com/neves6/piazzapanic/PiazzaPanicGame.java
+++ core/src/com/neves6/piazzapanic/PiazzaPanicGame.java
@@ -1,19 +1,8 @@
 package com.neves6.piazzapanic;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-
-import java.lang.reflect.Array;
-import java.util.List;
 
 public class PiazzaPanicGame extends Game {
 	SpriteBatch batch;
