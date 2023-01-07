46b75c655f30f6dd19da5f802d6a27c4d2c9c2ea
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-07 15:14:28 +0000

    Desktop config

--- desktop/src/com/neves6/piazzapanic/DesktopLauncher.java
+++ desktop/src/com/neves6/piazzapanic/DesktopLauncher.java
@@ -9,7 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("PiazzaPanic");
+		config.setTitle("Piazza Panic!");
+		config.useVsync(true);
 		new Lwjgl3Application(new PiazzaPanicGame(), config);
 	}
 }
