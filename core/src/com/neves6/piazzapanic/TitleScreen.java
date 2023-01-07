3e5026f2b08a76513bdf7e040b8bdcb5b90abdde
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-07 20:30:49 +0000

    Title screen background and title

--- core/src/com/neves6/piazzapanic/TitleScreen.java
+++ core/src/com/neves6/piazzapanic/TitleScreen.java
@@ -1,22 +1,32 @@
 package com.neves6.piazzapanic;
 
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TitleScreen extends ScreenAdapter {
     PiazzaPanicGame game;
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    BitmapFont font;
+    Texture bg;
+    int winWidth;
+    int winHeight;
+    float bgScaleFactor;
 
-    public TitleScreen(PiazzaPanicGame piazzaPanicGame) {
-        this.game = piazzaPanicGame;
+    public TitleScreen(PiazzaPanicGame game) {
+        this.game = game;
+        font = new BitmapFont(Gdx.files.internal("fonts/IBM_Plex_Mono_SemiBold.fnt"));
+        bg = new Texture(Gdx.files.internal("title_screen_large.png"));
     }
 
     @Override
     public void show(){
-        Gdx.input.setInputProcessor(new InputAdapter() {
+        /*Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean keyDown(int keyCode) {
                 if (keyCode == Input.Keys.SPACE) {
@@ -24,6 +34,45 @@ public class TitleScreen extends ScreenAdapter {
                 }
                 return true;
             }
-        });
+        });*/
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl20.glViewport( 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+
+        winWidth = Gdx.graphics.getWidth();
+        winHeight = Gdx.graphics.getHeight();
+        bgScaleFactor = (float) winHeight / (float) bg.getHeight();
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(
+                bg,
+                -((bg.getWidth() * bgScaleFactor) - winWidth) / 2,
+                0,
+                bg.getWidth() * bgScaleFactor,
+                bg.getHeight() * bgScaleFactor);
+        font.draw(game.batch, "TITLE SCREEN", winWidth / 2f - winWidth/10f, winHeight / 2f + winWidth/5f, winWidth/5f, 1, false);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        camera.setToOrtho(false, width, height);
+    }
+
+    @Override
+    public void hide(){
+        batch.dispose();
+        font.dispose();
+        bg.dispose();
     }
 }
