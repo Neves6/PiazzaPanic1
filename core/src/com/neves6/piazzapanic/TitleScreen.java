6546ffc7bbb96abc8e483355af2398325b2f4269
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-07 15:16:02 +0000

    Placeholder title screen

---
+++ core/src/com/neves6/piazzapanic/TitleScreen.java
@@ -0,0 +1,29 @@
+package com.neves6.piazzapanic;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+public class TitleScreen extends ScreenAdapter {
+    PiazzaPanicGame game;
+
+    public TitleScreen(PiazzaPanicGame piazzaPanicGame) {
+        this.game = piazzaPanicGame;
+    }
+
+    @Override
+    public void show(){
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keyCode) {
+                if (keyCode == Input.Keys.SPACE) {
+                    game.setScreen(new GameScreen(game));
+                }
+                return true;
+            }
+        });
+    }
+}
