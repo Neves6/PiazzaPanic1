0f5b6bc118f94bab0e6b613e995ce0a18122fbbc
Herma <49307226+hermaplusplus@users.noreply.github.com>
2023-01-21 16:13:55 +0000

    Game Screen setup alteration

--- core/src/com/neves6/piazzapanic/GameScreen.java
+++ core/src/com/neves6/piazzapanic/GameScreen.java
@@ -1,12 +1,68 @@
 package com.neves6.piazzapanic;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class GameScreen extends ScreenAdapter {
     PiazzaPanicGame game;
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    BitmapFont font;
+    Texture bg;
+    Stage stage;
+    int winWidth;
+    int winHeight;
 
     public GameScreen(PiazzaPanicGame game) {
         this.game = game;
+        font = new BitmapFont(Gdx.files.internal("fonts/IBM_Plex_Mono_SemiBold.fnt"));
+        bg = new Texture(Gdx.files.internal("title_screen_large.png"));
+    }
+
+    @Override
+    public void show(){
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
+
+        stage = new Stage();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl20.glViewport( 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        winWidth = Gdx.graphics.getWidth();
+        winHeight = Gdx.graphics.getHeight();
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.end();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        camera.setToOrtho(false, width, height);
+    }
+
+    @Override
+    public void hide(){
+
     }
 }
