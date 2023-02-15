44e2e705a5da60fde7c6066f7bdac3870fe46867
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-15 10:29:46 +0000

    Attempts to test

---
+++ tests/src/com/neves6/piazzapanic/tests/AssetTests.java
@@ -0,0 +1,16 @@
+package com.neves6.piazzapanic.tests;
+
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testShipAssetExists() {
+        assertTrue("TEST", 2 -1 == 1*1);
+    }
+}
