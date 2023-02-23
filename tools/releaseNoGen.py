21814af18af8cd666354f7ca9b867fe87adc4cb8
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 19:52:50 +0000

    Update file no

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -8,6 +8,8 @@ number = file.read()
 dotIndex = number.rfind(".") + 1
 newNo = int(number[dotIndex:]) + 1
 
+file.write(format(number[:dotIndex] + str(newNo))
+
 with open(env_file, "a") as myfile:
     myfile.write("STABLE_NAME={}".format(number[:dotIndex] + str(newNo)))
 
