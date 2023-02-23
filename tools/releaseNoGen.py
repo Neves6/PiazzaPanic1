3f1e1f7338e8aeb58f63e279d735fe226a7c0255
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 19:54:15 +0000

    Update file no - missing bracket

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -8,7 +8,7 @@ number = file.read()
 dotIndex = number.rfind(".") + 1
 newNo = int(number[dotIndex:]) + 1
 
-file.write(format(number[:dotIndex] + str(newNo))
+file.write(format(number[:dotIndex] + str(newNo)))
 
 with open(env_file, "a") as myfile:
     myfile.write("STABLE_NAME={}".format(number[:dotIndex] + str(newNo)))
