fd32a83f5cf0c3e41a8462c1b7a9bd1e42ae03cb
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 19:15:38 +0000

    Env instead for file naming - environment vars issue

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -1,8 +1,14 @@
+import os
+
+env_file = os.getenv('GITHUB_ENV')
+
 file = open(".github/workflows/releaseNumber.txt")
 number = file.read()
 
 dotIndex = number.rfind(".") + 1
 newNo = int(number[dotIndex:]) + 1
 
-return number[:dotIndex] + str(newNo)
+with open(env_file, "a") as myfile:
+    myfile.write("STABLE_NAME={}".format(number[:dotIndex] + str(newNo)))
+
 
