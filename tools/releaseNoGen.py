dbf49ba8ee207f3cb6ffe5537a29e9e4a13b1613
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 19:59:30 +0000

    Update file no - no write access

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -2,14 +2,16 @@ import os
 
 env_file = os.getenv('GITHUB_ENV')
 
-file = open(".github/workflows/releaseNumber.txt", "w")
+file = open(".github/workflows/releaseNumber.txt")
 number = file.read()
+file.close()
 
 dotIndex = number.rfind(".") + 1
 newNo = int(number[dotIndex:]) + 1
 
+with open(".github/workflows/releaseNumber.txt") as file:
+    file.write(format(number[:dotIndex] + str(newNo)))
 
-file.write(format(number[:dotIndex] + str(newNo)))
 
 with open(env_file, "a") as myfile:
     myfile.write("STABLE_NAME={}".format(number[:dotIndex] + str(newNo)))
