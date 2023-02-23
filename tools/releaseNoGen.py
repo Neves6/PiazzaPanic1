5e8b251e3487856b6107b63b28609d9123c037ce
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 22:07:18 +0000

    Use date and time instead for workflow

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+env_file = os.getenv('GITHUB_ENV')
+
 base_date = date(2023, 3, 2)
 today_date = date.today()
 
@@ -8,6 +10,6 @@ releaseNo = delta.days // 7 + 1
 
 
 with open(env_file, "a") as myfile:
-    myfile.write("STABLE_NAME={{Stable/v1.1.{}}}".format(releaseNo))
+    myfile.write("STABLE_NAME={{Stable/v1.1.{}}}".format(releaseNo)))
 
 
