ab76b2aa2b36735c48c9f95c8e07dc0479c16da9
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-23 19:56:12 +0000

    Update file no - no write access

--- tools/releaseNoGen.py
+++ tools/releaseNoGen.py
@@ -2,11 +2,12 @@ import os
 
 env_file = os.getenv('GITHUB_ENV')
 
-file = open(".github/workflows/releaseNumber.txt")
+file = open(".github/workflows/releaseNumber.txt", "w")
 number = file.read()
 
 dotIndex = number.rfind(".") + 1
 newNo = int(number[dotIndex:]) + 1
+
 
 file.write(format(number[:dotIndex] + str(newNo)))
 
