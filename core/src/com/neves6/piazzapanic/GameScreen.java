899b1414cf8d9d26ea1a9818518517857165e66a
Milo Johnson <mijajoh@gmail.com>
2023-02-25 21:41:18 +0000

    Added more fridges to Tiled map

--- core/src/com/neves6/piazzapanic/GameScreen.java
+++ core/src/com/neves6/piazzapanic/GameScreen.java
@@ -125,7 +125,7 @@ public class GameScreen extends ScreenAdapter {
         font.draw(game.batch, gm.getMachineTimerForChef(0), gm.getChef(1).getxCoord() * wScale, gm.getChef(1).getyCoord() * hScale + 2*(hScale/3f), 32 * unitScale, 1, false);
         font.draw(game.batch, gm.getMachineTimerForChef(1), gm.getChef(2).getxCoord() * wScale, gm.getChef(2).getyCoord() * hScale + 2*(hScale/3f), 32 * unitScale, 1, false);
         game.batch.draw(recipes, 20, 20);
-        font.draw(game.batch, gm.generateHoldingsText(), winWidth - (6*(winWidth/8f)), winHeight - 20, (3*(winWidth/8f)), -1, true);
+        font.draw(game.batch, gm.generateHoldingsText(), winWidth - (4.75f*(winWidth/8f)), winHeight - 20, (3*(winWidth/8f)), -1, true);
         font.draw(game.batch, gm.generateCustomersTrayText(), winWidth - (3*(winWidth/8f)), winHeight - 20, (3*(winWidth/8f)), -1, true);
         font.draw(game.batch, gm.generateTimerText(), winWidth - (winWidth/3f), 40, (winWidth/3f), -1, false);
         game.batch.end();
