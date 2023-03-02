d89f452fc6d489589fb4a091eea646ccc2046306
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-03-02 13:55:55 +0000

    All docstrings done

--- core/src/com/neves6/piazzapanic/powerups/DoubleSpeed.java
+++ core/src/com/neves6/piazzapanic/powerups/DoubleSpeed.java
@@ -4,12 +4,25 @@ import com.neves6.piazzapanic.Chef;
 
 import java.util.ArrayList;
 
+/***
+ * Allows the chefs to go double speed for a given time.
+ */
 public class DoubleSpeed extends BasePowerUp{
 
+    /***
+     * Constructor.
+     * @param effectTime How long the effect will last for.
+     */
     public DoubleSpeed(Long effectTime) {
         super(effectTime);
     }
 
+    /***
+     * Takes all active chefs in the game and changes their speed to
+     * if the power up is acquired.
+     * @param chefs List of all active chefs.
+     * @return Chefs with modified speed.
+     */
     public ArrayList<Chef> applyPowerUp(ArrayList<Chef> chefs) {
         if (chefs.size() < 1){
             throw new IllegalArgumentException("Chefs list must have at least two chefs in.");
@@ -23,6 +36,12 @@ public class DoubleSpeed extends BasePowerUp{
         return chefs;
     }
 
+    /***
+     * Takes all active chefs in the game and changes their speed to
+     * normal if the power up has ended.
+     * @param chefs List of all active chefs.
+     * @return Chefs with normal speed.
+     */
     public ArrayList<Chef> endPowerUp(ArrayList<Chef> chefs) {
         if (chefs.size() < 1){
             throw new IllegalArgumentException("Chefs list must have at least two chefs in.");
--- core/src/com/neves6/piazzapanic/powerups/ShorterMachineTime.java
+++ core/src/com/neves6/piazzapanic/powerups/ShorterMachineTime.java
@@ -4,11 +4,26 @@ import com.neves6.piazzapanic.Machine;
 
 import java.util.ArrayList;
 
+/***
+ * A power up which makes food take a shorter amount of time to cook.
+ */
 public class ShorterMachineTime extends BasePowerUp{
+
+    /***
+     * Constructor
+     *
+     * @param effectTime How long the power up lasts.
+     * */
     public ShorterMachineTime(Long effectTime) {
         super(effectTime);
     }
 
+    /***
+     * Takes all active machines in the game and changes their processing speed
+     * if the power up has ended.
+     * @param machines List of all active machines.
+     * @return Machines with decreasing processing speed.
+     */
     public ArrayList<Machine> applyPowerUp(ArrayList<Machine> machines) {
         if (machines.size() < 1){
             throw new IllegalArgumentException("Chefs list must have at least two chefs in.");
@@ -22,6 +37,12 @@ public class ShorterMachineTime extends BasePowerUp{
         return machines;
     }
 
+    /***
+     * Takes all active machines in the game and changes their processing speed to
+     * normal if the power up has ended.
+     * @param machines List of all active machines.
+     * @return Machines with normal processing speed.
+     */
     public ArrayList<Machine> endPowerUp(ArrayList<Machine> machines) {
         if (machines.size() < 1){
             throw new IllegalArgumentException("Chefs list must have at least two chefs in.");
