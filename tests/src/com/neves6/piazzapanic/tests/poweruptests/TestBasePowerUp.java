15f178268b0f4b95f33a3ca06cbb5631beb92a07
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-04-02 12:43:22 +0100

    Changed method names

--- tests/src/com/neves6/piazzapanic/tests/poweruptests/TestBasePowerUp.java
+++ tests/src/com/neves6/piazzapanic/tests/poweruptests/TestBasePowerUp.java
@@ -13,30 +13,19 @@ public class TestBasePowerUp {
   BasePowerUp testPowerUp = new BasePowerUp(1L, "Test Powerup");
 
   @Test
-  public void testConstructorI() {
-    assertFalse("Constructor should not modify acquired status", testPowerUp.getAcquiredStatus());
-  }
-
-  @Test
-  public void testConstructorII() {
+  public void testConstructor() {
     assertEquals("Constructor should not modify start time", 0L, (long) testPowerUp.getStartTime());
   }
 
   @Test
-  public void testConstructorIII() {
-    assertEquals(
-        "Constructor should not modify effect time", 1L, (long) testPowerUp.getEffectTime());
-  }
-
-  @Test
-  public void validActivation() {
+  public void testValidActivation() {
     testPowerUp.setStartTime();
     assertTrue(
         "Start time should be set if power-up is attained.", testPowerUp.getStartTime() != 0L);
   }
 
   @Test
-  public void validEndTime() throws InterruptedException {
+  public void testValidEndTime() throws InterruptedException {
     testPowerUp.acquirePowerUp();
     testPowerUp.setStartTime();
     TimeUnit.MILLISECONDS.sleep(5);
@@ -46,18 +35,18 @@ public class TestBasePowerUp {
   BasePowerUp testPowerUpII = new BasePowerUp(5000L, "Test powerup #2");
 
   @Test
-  public void invalidEndTime() {
+  public void testInvalidEndTime() {
     assertFalse(
         "End time should not change anything if it was never activated ", testPowerUpII.endTime());
   }
 
   @Test
-  public void inactivePrettyPrint() {
+  public void testInactivePrettyPrint() {
     assertEquals("Inactive powerup should not return a message", "", testPowerUp.prettyPrint());
   }
 
   @Test
-  public void activePrettyPrint() {
+  public void testActivePrettyPrint() {
     testPowerUpII.acquirePowerUp();
     assertFalse("String must contain the name of the powerup", testPowerUpII.prettyPrint() == "");
     assertTrue(testPowerUpII.prettyPrint().contains("Test powerup #2:"));
